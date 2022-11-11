1109b568c0dc126d683a9db575fd9c807d04beb4
Julia1399 <84142073+Julia1399@users.noreply.github.com>
2022-11-11 11:25:55 +0300

    Create 69_Sqrt(x).js

---
+++ 69_Sqrt(x).js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    for (var i=0; i<=x;i++){
+        if (i*i === x){
+            return i
+        }
+        if ((i*i < x) & ((i+1)*(i+1) >x)){
+            return i
+        }
+    }
+    return 0
+};
